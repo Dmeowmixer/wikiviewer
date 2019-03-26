f121475b7756f84a987a4a135367153bd205f315
Dmeowmixer <ransonikuo@gmail.com>
2019-03-26 07:21:32 -1000

    includes httpsparams to app.component and filters get req successfully

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,5 +1,7 @@
 import { Component } from '@angular/core';
-import { HttpClient } from '@angular/common/http'
+import { HttpClient, HttpParams } from '@angular/common/http';
+import { Post } from './post';
+import { Observable } from 'rxjs/Observable';
 
 @Component({
   selector: 'app-root',
@@ -8,13 +10,16 @@ import { HttpClient } from '@angular/common/http'
 })
 export class AppComponent {
   title = 'wikipedia-viewer';
+
   readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
 
-  posts: any;
+  posts: Observable<any>;
 
   constructor(private http: HttpClient) {}
 
   getPosts(){
-    this.posts = this.http.get(this.ROOT_URL + '/posts');
+    let params = new HttpParams().set('userId', '1');
+    this.posts = this.http.get(this.ROOT_URL + '/posts', { params })
+
   }
 }
