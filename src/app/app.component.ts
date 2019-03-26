e157861591d2f8c7f62a7fdb853d0423579ea30e
Dmeowmixer <ransonikuo@gmail.com>
2019-03-26 13:33:53 -1000

    reverting back to pre-seach component/folder to debu and work around error (wikimedia returning non-json formatted data)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -8,18 +8,22 @@ import { Observable } from 'rxjs/Observable';
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
-export class AppComponent {
-  title = 'wikipedia-viewer';
 
-  readonly ROOT_URL = 'https://jsonplaceholder.typicode.com';
+export class AppComponent {
+  title = 'Wiki Search';
+  // readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=japan&origin=*&format=json';
+  readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=Harry Potter&origin=*&callback=JSON_CALLBACK';
+  // https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=japan"
+
 
   posts: Observable<any>;
 
   constructor(private http: HttpClient) {}
 
   getPosts(){
-    let params = new HttpParams().set('userId', '1');
-    this.posts = this.http.get(this.ROOT_URL + '/posts', { params })
-
+    this.posts = this.http.get(this.ROOT_URL)
   }
 }
+
+
+// this.httpClient.post(this.apiURL + '/login', user, {headers:reqHeader, responseType: 'text'});
