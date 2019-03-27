e6ed08d85b7fdd2cbfcb4b5714774205ed55d07c
Dmeowmixer <ransonikuo@gmail.com>
2019-03-27 13:42:28 -1000

    creating subscription to http req, pushing to array and displaying with ngFor. Currently have subscription written, debugging typesetting error

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,6 +1,6 @@
 import { Component } from '@angular/core';
 import { HttpClient, HttpParams } from '@angular/common/http';
-import { Post } from './post';
+// import { Post } from './post';
 import { Observable } from 'rxjs/Observable';
 
 @Component({
@@ -10,20 +10,27 @@ import { Observable } from 'rxjs/Observable';
 })
 
 export class AppComponent {
+  private data:Observable<Array<number>>;
+  private values: Array<number> = [];
+  private anyErrors:boolean;
+  private finished: boolean;
+
   title = 'Wiki Search';
-  // readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=japan&origin=*&format=json';
-  readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=Harry Potter&origin=*';
+  readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?action=query&list=search&srsearch=japan&origin=*&format=json';
+  // readonly ROOT_URL = 'https://en.wikipedia.org/w/api.php?format=json&action=query&generator=search&gsrnamespace=0&gsrlimit=10&prop=pageimages|extracts&pilimit=max&exintro&explaintext&exsentences=1&exlimit=max&gsrsearch=Harry Potter&origin=*';
   // https://en.wikipedia.org/w/api.php?action=query&format=json&gsrlimit=15&generator=search&origin=*&gsrsearch=japan"
 
 
-  posts: Observable<any>;
+  // posts: Observable<any>;
 
-  constructor(private http: HttpClient) {}
+  constructor() {}
 
   getPosts(){
-    this.posts = this.http.get(this.ROOT_URL)
+    this.data = new Observable(observer => {
+    });
+    let subscription = this.data.subscribe( value => this.values.push(value),
+      error => this.anyErrors = true,
+      () => this.finished = true );
+    // this.posts = this.http.get(this.ROOT_URL)
   }
 }
-
-
-// this.httpClient.post(this.apiURL + '/login', user, {headers:reqHeader, responseType: 'text'});
